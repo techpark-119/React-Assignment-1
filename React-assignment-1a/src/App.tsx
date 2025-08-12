49fd368cee4a31b90cc58a00d0b19764f76bcc2b
techpark-119 <techpark-119@protonmail.com>
2025-08-12 23:29:40 +0000

    V: 1.0

---
+++ React-assignment-1a/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import FAQ from './components/FAQ'
+import Newsletter from './components/Newsletter'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <FAQ />
+      <Newsletter />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
